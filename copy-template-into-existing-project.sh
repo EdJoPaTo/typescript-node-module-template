7c7226e8d49aaaeb109413434ba3d9bdef6892da
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-06-20 07:03:05 +0200

    chore(copy): add helper script to use template

---
+++ copy-template-into-existing-project.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+# Usage
+# Go to the project you want to improve via this template
+# cd ~/git/my-project
+# Run this script from the working directory of that project
+# ~/git/typescript-node-module-boilerplate/copy-template-into-existing-project.sh
+
+pwd=$(pwd)
+name=$(basename "$pwd")
+
+cd "$(dirname "$0")"
+
+cp -rv \
+    {package.json,tsconfig.json,.gitignore,.github} \
+    "$pwd"
+
+echo "everything copied"
+
+cd -
+
+# Replace template name with folder name
+# macOS: add '' after -i like this: sed -i '' "s/…
+sed -i "s/typescript-node-module-boilerplate/$name/g" package.json .github/**/*.yml
+
+git --no-pager diff --stat
