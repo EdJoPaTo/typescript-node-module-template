f3208928615517cca90868ff7067648c00c5b82a
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2021-08-09 08:33:25 +0200

    refactor(xo): adapt to new rules

--- test/tests-for-javascript-users.ts
+++ test/tests-for-javascript-users.ts
@@ -3,7 +3,7 @@ import moduleName from '../source';
 
 test('title', t => {
 	t.throws(() => {
-		// @ts-expect-error
+		// @ts-expect-error moduleName expects a string not a number
 		moduleName(123);
 	}, {
 		message: 'Expected a string, got number',
