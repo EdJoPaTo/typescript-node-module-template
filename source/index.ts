f76ffcd59f52412864c366b7cdae6e723984a198
EdJoPaTo <rfc-conform-git-commit-email@funny-long-domain-label-everyone-hates-as-it-is-too-long.edjopato.de>
2023-08-29 12:43:23 +0200

    feat: example with optional chaining
    optional chaining and nullish coalescing (??) are great.
    TypeScript target version could be used to support older versions.
    This example code is helpful to see this in effect and decide better on
    the target version.

--- source/index.ts
+++ source/index.ts
@@ -1,7 +1,8 @@
-export function moduleName(input: string): string {
-	if (typeof input !== 'string') {
+export function moduleName(input: string | undefined): string {
+	if (typeof input !== 'string' && input !== undefined) {
 		throw new TypeError(`Expected a string, got ${typeof input}`);
 	}
 
-	return input + ' & rainbows';
+	const sanetized = input?.trim() ?? 'Glitter';
+	return sanetized + ' & rainbows';
 }
